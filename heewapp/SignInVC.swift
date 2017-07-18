75ae6ab12da68669cc89b1233903da5d2a05b92e
Natthaphat <natthaphat.itthiwaranurak@gmail.com>
2017-07-18 23:43:35 +0700

    Completed Facebook Authentication

--- heewapp/SignInVC.swift
+++ heewapp/SignInVC.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //FACEBOOK AUTHENTICATION
+    @IBAction func facebookBtnPressed(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) {(result, error) in
+            
+            if error != nil {
+                print("GUIDE: Unable to authenticate with Facebook -\(error)")
+            } else if result?.isCancelled == true {
+                print("GUIDE: User   authenticate with Facebook")
+            } else {
+                print("GUIDE: Successfully authenticate with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
     }
-
-
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: {(user, error) in
+            
+            if error != nil {
+                print("GUIDE: Unable to authenticate with Firebase/Facebook -\(error)")
+            } else {
+                print("GUIDE: Successfully authenticate with Firebase/Facebook")
+            }
+        })
+    }
+        
 }
 
